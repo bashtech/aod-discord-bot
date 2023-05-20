99b7ab04da83138b77c9b48af5a5c12be3fbbb13
Adam Schultz <adam.schultz@live.com>
2023-05-20 16:10:57 -0400

    Fix roll slash command

--- commands/roll.js
+++ commands/roll.js
@@ -6,11 +6,11 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('roll')
 		.setDescription('Roll a dice.')
-		.addIntegerOption(option => option.setName('dice').setDescription('The number of dice to roll').setMinValue(1).setMaxValue(20))
-		.addIntegerOption(option => option.setName('sides').setDescription('The number of sides on the dice').setMinValue(1).setMaxValue(100)),
+		.addIntegerOption(option => option.setName('dice').setDescription('The number of dice to roll (1-20)').setMinValue(1).setMaxValue(20))
+		.addIntegerOption(option => option.setName('sides').setDescription('The number of sides on the dice (1-100)').setMinValue(1).setMaxValue(100)),
 	async execute(interaction) {
 		const num = interaction.options.getInteger('dice') ?? 1;
-		const size = interaction.options.getInteger('size') ?? 6;
+		const size = interaction.options.getInteger('sides') ?? 6;
 		let reply = `${num}d${size} result: `;
 		let total = 0;
 		for (let i = 0; i < num; i++) {
