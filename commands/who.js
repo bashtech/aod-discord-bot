83cf61c90a6e7d48d97ed4d05f48598c218cc36f
Adam Schultz <adam.schultz@live.com>
2024-04-14 11:44:18 -0400

    Add moderator buttons to who command; allow officers to move people within the division

--- commands/who.js
+++ commands/who.js
@@ -12,6 +12,118 @@ const {
 	ButtonStyle
 } = require('discord.js');
 
+function getComponentsForTarget(member, perm, targetMember, targetPerm, invite) {
+	let components = [];
+	if (member.id === targetMember.id) {
+		return components;
+	}
+	if (member.voice.channel && member.voice.channelId !== targetMember.voice.channelId) {
+		const row = new ActionRowBuilder();
+		//create invite button
+		if (invite === true) {
+			const invite = new ButtonBuilder()
+				.setCustomId('send_invite')
+				.setLabel('Invite to your channel')
+				.setStyle(ButtonStyle.Primary);
+			row.addComponents(invite);
+		}
+
+		//if the target is in a voie channel, check if member can move them
+		if (targetMember.voice.channel) {
+			let canMove = member.permissions.has(PermissionsBitField.Flags.MoveMembers);
+			if (!canMove && member.voice.channel.id === targetMember.voice.channel.id) {
+				let catgory = member.voice.channel.parent;
+				let officerRole;
+				if (catgory) {
+					//check if this category has an associated officer role
+					let officerRoleName = category.name + ' ' + global.config.discordOfficerSuffix;
+					officerRole = interaction.guild.roles.cache.find(r => { return r.name == officerRoleName; });
+				}
+				if (officerRole && member.roles.resolve(officerRole.id)) {
+					canMove = true;
+				}
+			}
+			//if member has permission to move target, create a move button
+			if (canMove) {
+				const move = new ButtonBuilder()
+					.setCustomId('move_to_me')
+					.setLabel('Move to your channel')
+					.setStyle(ButtonStyle.Danger);
+				row.addComponents(move);
+			}
+		}
+
+		if (row.components.length) {
+			components.push(row);
+		}
+	}
+
+	if (perm >= global.PERM_MOD && perm > targetPerm) {
+		const modRow = new ActionRowBuilder();
+		if (targetMember.voice.channel) {
+			if (targetMember.voice.serverMute) {
+				const unmute = new ButtonBuilder()
+					.setCustomId('server_unmute')
+					.setLabel('Unmute Voice')
+					.setStyle(ButtonStyle.Primary);
+				modRow.addComponents(unmute);
+			} else {
+				const mute = new ButtonBuilder()
+					.setCustomId('server_mute')
+					.setLabel('Mute Voice')
+					.setStyle(ButtonStyle.Danger);
+				modRow.addComponents(mute);
+			}
+
+			if (targetMember.voice.serverDeaf) {
+				const unmute = new ButtonBuilder()
+					.setCustomId('server_undeaf')
+					.setLabel('Undeafen')
+					.setStyle(ButtonStyle.Primary);
+				modRow.addComponents(unmute);
+			} else {
+				const mute = new ButtonBuilder()
+					.setCustomId('server_deaf')
+					.setLabel('Deafen')
+					.setStyle(ButtonStyle.Danger);
+				modRow.addComponents(mute);
+			}
+		}
+
+		let muteRole = targetMember.roles.cache.find(r => r.name === global.config.muteRole);
+		if (muteRole) {
+			const unmute = new ButtonBuilder()
+				.setCustomId('remove_mute_role')
+				.setLabel('Remove Mute Role')
+				.setStyle(ButtonStyle.Primary);
+			modRow.addComponents(unmute);
+		} else {
+			const mute = new ButtonBuilder()
+				.setCustomId('add_mute_role')
+				.setLabel('Add Mute Role')
+				.setStyle(ButtonStyle.Danger);
+			modRow.addComponents(mute);
+		}
+
+		let pttRole = targetMember.roles.cache.find(r => r.name === global.config.pttRole);
+		if (pttRole) {
+			const unsetptt = new ButtonBuilder()
+				.setCustomId('remove_ptt_role')
+				.setLabel('Remove PTT Role')
+				.setStyle(ButtonStyle.Primary);
+			modRow.addComponents(unsetptt);
+		} else {
+			const setptt = new ButtonBuilder()
+				.setCustomId('add_ptt_role')
+				.setLabel('Add PTT Role')
+				.setStyle(ButtonStyle.Danger);
+			modRow.addComponents(setptt);
+		}
+		components.push(modRow);
+	}
+	return components;
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('who')
@@ -28,22 +140,22 @@ module.exports = {
 	},
 	async execute(interaction, member, perm, permName) {
 		await interaction.deferReply({ ephemeral: true });
-		
+
 		let targetMember = interaction.options.getMember('user');
 		const userData = await global.getForumInfoForMember(targetMember);
 		const memberRole = interaction.guild.roles.cache.find(r => { return r.name == global.config.memberRole; });
-		
+
 		let embed = {
 			description: `**Information for ${targetMember}**`,
-			thumbnail: { url: targetMember.displayAvatarURL({extension: 'png'}) },
+			thumbnail: { url: targetMember.displayAvatarURL({ extension: 'png' }) },
 			fields: []
 		};
-		
+
 		embed.fields.push({
 			name: 'Discord User',
 			value: `${targetMember.user.username} (${targetMember.id})`
 		});
-		
+
 		if (!userData || userData.length == 0) {
 			embed.fields.push({
 				name: 'Forum Data',
@@ -54,8 +166,7 @@ module.exports = {
 				let data = userData[i];
 				embed.fields.push({
 					name: 'Forum Data',
-					value: 
-						`**Username**: ${data.name} (${data.id})\n` +
+					value: `**Username**: ${data.name} (${data.id})\n` +
 						`**Division**: ${data.division}\n` +
 						`**Rank**: ${data.rank}\n` +
 						`**Status**: ${data.loaStatus}\n`
@@ -80,60 +191,86 @@ module.exports = {
 			name: 'Roles',
 			value: targetMember.roles.cache
 				.filter(r => r != interaction.guild.roles.everyone)
-				.sort((r1,r2) => r2.position - r1.position)
+				.sort((r1, r2) => r2.position - r1.position)
 				.map(r => `${r}`)
 				.join(', ')
 		});
 
-		let components = [];
-		if (member.voice.channel && member.voice.channelId !== targetMember.voice.channelId) {
-			const row = new ActionRowBuilder();
-			const invite = new ButtonBuilder()
-				.setCustomId('send_invite')
-				.setLabel('Invite to your channel')
-				.setStyle(ButtonStyle.Primary);
-			row.addComponents(invite);
-			if (member.permissions.has(PermissionsBitField.Flags.MoveMembers)) {
-				const move = new ButtonBuilder()
-					.setCustomId('move_to_me')
-					.setLabel('Move to your channel')
-					.setStyle(ButtonStyle.Danger);
-				row.addComponents(move);
-			}
-			components.push(row);
-		}
+		let [targetPerm, targetPermName] = getPermissionLevelForMember(targetMember);
+		let components = getComponentsForTarget(member, perm, targetMember, targetPerm, true);
 
 		interaction.replied = true; //avoid common reply
-		const response = await global.ephemeralReply(interaction, { embeds: [embed], components: components}, true);
+		const response = await global.ephemeralReply(interaction, { embeds: [embed], components: components }, true);
 		if (components.length) {
-			const filter = (i) => (i.customId === 'send_invite' || i.customId === 'move_to_me') && i.user.id === interaction.user.id;
+			const buttons = ['send_invite', 'move_to_me', 'server_unmute', 'server_mute', 'server_undeaf', 'server_deaf',
+				'remove_mute_role', 'add_mute_role', 'remove_ptt_role', 'add_ptt_role'];
+			const filter = (i) => i.user.id === interaction.user.id && buttons.includes(i.customId);
 			try {
-				const confirmation = await response.awaitMessageComponent({ filter: filter, time: 10000 });
-				if (confirmation.customId === 'send_invite') {
-					confirmation.update({
-						components: []
-					});
-					let invite = await member.voice.channel.createInvite({
-						maxAge: 5 * 60, /* 5 minutes */
-						maxUses: 1,
-						temporary: true,
-						reason: `Requested by ${global.getNameFromMessage(interaction)}`
-					});
-					if (invite) {
-						await targetMember.send(`${member} has invited you to their voice channel: ${invite.url}`);
-						await global.ephemeralReply(interaction, {content: 'Intivation sent'});
-					} else {
-						await global.ephemeralReply(interaction, {content: 'Failed to create invitation.'});
+				while (1) {
+					const confirmation = await response.awaitMessageComponent({ filter: filter, time: 15000 });
+					switch (confirmation.customId) {
+						case 'send_invite': {
+							let invite = await member.voice.channel.createInvite({
+								maxAge: 5 * 60, //5 minutes
+								maxUses: 1,
+								temporary: true,
+								reason: `Requested by ${global.getNameFromMessage(interaction)}`
+							});
+							if (invite) {
+								await targetMember.send(`${member} has invited you to their voice channel: ${invite.url}`);
+								global.ephemeralReply(interaction, { content: 'Intivation sent' });
+							} else {
+								global.ephemeralReply(interaction, { content: 'Failed to create invitation.' });
+							}
+							break;
+						}
+						case 'move_to_me': {
+							confirmation.update({ components: [] });
+							await targetMember.voice.setChannel(member.voice.channelId);
+							global.ephemeralReply(interaction, { content: `${targetMember} moved to your channel.` });
+							break;
+						}
+						case 'server_unmute': {
+							await targetMember.voice.setMute(false, `Requested by ${global.getNameFromMessage(interaction)}`).catch(console.log);
+							break;
+						}
+						case 'server_mute': {
+							await targetMember.voice.setMute(true, `Requested by ${global.getNameFromMessage(interaction)}`).catch(console.log);
+							break;
+						}
+						case 'server_undeaf': {
+							await targetMember.voice.setDeaf(false, `Requested by ${global.getNameFromMessage(interaction)}`).catch(console.log);
+							break;
+						}
+						case 'server_deaf': {
+							await targetMember.voice.setDeaf(true, `Requested by ${global.getNameFromMessage(interaction)}`).catch(console.log);
+							break;
+						}
+						case 'remove_mute_role': {
+							await addRemoveRole(interaction, interaction.guild, false, global.config.muteRole, targetMember, true);
+							break;
+						}
+						case 'add_mute_role': {
+							await addRemoveRole(interaction, interaction.guild, true, global.config.muteRole, targetMember, true);
+							break;
+						}
+						case 'remove_ptt_role': {
+							await addRemoveRole(interaction, interaction.guild, false, global.config.pttRole, targetMember, true);
+							break;
+						}
+						case 'add_ptt_role': {
+							await addRemoveRole(interaction, interaction.guild, true, global.config.pttRole, targetMember, true);
+							break;
+						}
 					}
-				} else if (confirmation.customId === 'move_to_me') {
-					confirmation.update({
-						components: []
-					});
-					await targetMember.voice.setChannel(member.voice.channelId);
-					await global.ephemeralReply(interaction, {content: `${targetMember} moved to your channel.`});
+					components = getComponentsForTarget(member, perm, targetMember, targetPerm);
+					confirmation.update({ components: components });
+					if (!components.length) {
+						break;
+					}
 				}
 			} catch (e) {
-				await global.ephemeralReply(interaction, {components: []}, true);
+				global.ephemeralReply(interaction, { components: [] }, true);
 			}
 		}
 	},
